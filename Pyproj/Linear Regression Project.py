45b8c624854475adae0ec17bd67c78e9e2e575e9
Alec Dosier <dosiera@gmail.com>
2020-03-04 14:58:12 -0600

    finished Rproj, received additional information for Pyproj and finalized everything.

--- Pyproj/Linear Regression Project.py
+++ Pyproj/Linear Regression Project.py
@@ -1,9 +1,16 @@
+# Name: Alec Dosier
+# NetID: ald170830
+# Date: 3/1/2020
+# Class: 4375.502 Introduction to Machine Learning
+# -------------------------------------------------
+
 # Import pandas, numpy, matplotlib, and seaborn.
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
-
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 # --------------- ENVIRONMENT CONFIGUREATION
 # This allows functions like head() to display more than a single column in the console
 pd.set_option('display.max_columns', None)
@@ -21,10 +28,9 @@ print(customers.describe())
 
 # Use seaborn to create a jointplot to compare the Time on Website and Yearly Amount Spent columns. Does the correlation make sense?
 
-# The data shows almost no obvious correlation,
-# most people in this dataset spend an average amount of time and an average amount of money
+#   The data shows almost no obvious correlation,
+#   most people in this dataset spend an average amount of time and an average amount of money
 sns.jointplot(x="Time on Website", y="Yearly Amount Spent", data=customers)
-
 
 # Do the same but with the Time on App column instead.
 # this jointplot shows a clear positive correlation between the time on app and money spent
@@ -35,28 +41,27 @@ sns.jointplot(x="Time on App", y="Yearly Amount Spent", data=customers)
 sns.jointplot(x="Time on App", y="Length of Membership", data=customers, kind='hex')
 
 
-# Let's explore these types of relationships across the entire data set. Use pairplot to recreate the plot below.(Don't worry about the the colors)
-
-#????? what plot
+# Let's explore these types of relationships across the entire data set. Use pairplot to recreate the plot below.
+# (Don't worry about the the colors)
+sns.pairplot(customers)
 
 # Based off this plot what looks to be the most correlated feature with Yearly Amount Spent?
-
-# NULL
+#   length of membership
 
 # Create a linear model plot(using seaborn's lmplot) of Yearly Amount Spent vs. Length of Membership.
 p = sns.lmplot(x="Length of Membership", y="Yearly Amount Spent", data=customers)
-#plt.show()
 
 # customers and a variable y equal to the "Yearly Amount Spent" column.
-# ?
+# Now that we've explored the data a bit, let's go ahead and split the data into training and
+# testing sets. ** Set a variable X equal to the numerical features of the customers and
+# a variable y equal to the "Yearly Amount Spent" column.
 
-# Use model_selection.train_test_split from sklearn to split the data into training and testing sets. Set test_size=0.3 and random_state=101
-
-# to use model_selection I have to import it from sklearn first
-from sklearn.model_selection import train_test_split
-# the instructions are pretty vague so I'll just assume we are trying to find what makes ammount spent go up
 X = customers[['Avg. Session Length', 'Time on App', 'Time on Website', 'Length of Membership']]
 y = customers['Yearly Amount Spent']
+
+# Use model_selection.train_test_split from sklearn to split the data into training and testing sets.
+# Set test_size=0.3 and random_state=101
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
 
 # Import LinearRegression from sklearn.linear_model
@@ -69,27 +74,61 @@ lm = LinearRegression()
 lm.fit(X_train, y_train)
 
 # Print out the coefficients of the model
-print(pd.DataFrame(lm.coef_, X.columns, columns=['Coefficents']))
+print("MODEL COEFFICENTS =======================")
+#   I assigned this to a df because it is the answer to the "recreate this dataframe" question later
+modcoef = pd.DataFrame(lm.coef_, X.columns, columns=['Coefficents'])
+print(modcoef)
 
 # Use lm.predict() to predict off the X_test set of the data.
-
+prediction = lm.predict(X_test)
 
 # Create a scatterplot of the real test values versus the predicted values.
-
+plt.scatter(y_test, prediction)
+plt.show()
 
 # Calculate the Mean Absolute Error, Mean Squared Error, and the Root Mean Squared Error. Refer to the lecture or to Wikipedia for the formulas
-
+MAE = metrics.mean_absolute_error(y_test, prediction)  # this finds the average error between predicted an actual
+MSE = metrics.mean_squared_error(y_test, prediction)
+RMSE = np.sqrt(MSE)
+print('MAE: ', MAE)
+print('MSE: ', MSE)
+print('RMSE: ', RMSE)
 
 # Plot a histogram of the residuals and make sure it looks normally distributed. Use either seaborn distplot, or just plt.hist().
-
-
+sns.distplot((y_test-prediction), bins=50)
+plt.show()
 # Recreate the dataframe below.
+# ================================
+#                       Coefficent|
+# Avg. Session Length   25.981550 |
+# Time on App           38.590159 |
+# Time on Website        0.190405 |
+# Length of Membership  61.279097 |
+# ================================
 
+#   This dataframe is already exists in "modcoef"
+print("recreation of the dataframe given to us ------------------")
+print(modcoef)
 
 # How can you interpret these coefficients?
-
+#   These coefficients show how much the Yearly Ammount Spent is adjusted for every 1 unit that a feature increases
+#   They also are an indicator of correlation between the target and the feature
+# ================================
+#                       Coefficent|
+# Avg. Session Length   25.981550 |
+# Time on App           38.590159 |
+# Time on Website        0.190405 | * This shows no significant correlation
+# Length of Membership  61.279097 |
+# ================================
 
 # Do you think the company should focus more on their mobile app or on their website?
+#   This question depends on what the context of the word "focus" is.
+#   Based on the data above, their revenue comes from their time spent on the app.
+#       You could say that they should focus on their app because it brings money in.
+#   However, their Website is worthless from a revenue perspective.
+#       If their website is meant to make sales, they need to focus on that so they can have
+#       two sources of revenue. They could make their app perform worse if they keep adding superficial features to it.
 
+#   The answer is based on the context of the business and not a choice that should be made purely from the data.
 
 # Ref:www.pieriandata.com
--- RProj/04 - Linear Regression Project.R
+++ RProj/04 - Linear Regression Project.R
@@ -1,6 +1,14 @@
+# ===============================
+# Name: Alec Dosier
+# NetID: ald170830
+# Date: 3/1/2020
+# Class: 4375.502 Intro To Machine Learning
+# ===============================
+
 ###
 # Linear Regression Project
 # 1.  Read in bikeshare.csv file and set it to a dataframe called bike. 
+bike = read.csv('bikeshare.csv')
 
 ### Bikshare Data Set Information
 # . datetime - hourly date + timestamp 
@@ -23,20 +31,49 @@
 ###
 
 # 2.  Check the head of df
+
+  # I am guessing we need to predict the number of total rentals (count)
+  # because if someone hires a data scientitst, they want you to make them more money from data.
+
 # 3.  Can you figure out what is the target we are trying to predict? 
 #     Check the Kaggle Link above if you are confused on this. 
+
+head(bike)
+
 
 ### 
 # Exploratory Data Analysis
 
 # 4.  Create a scatter plot of count vs temp. Set a good alpha value.
+library(ggplot2)
+qplot(temp, count, data=bike, alpha=0.1)
+
 # 5.  Plot count versus datetime as a scatterplot with a color gradient 
 #     based on temperature. You'll need to convert the datetime column 
 #     into POSIXct before plotting.
 
+# conversion of datetime col to POSIXct
+# First I have to convert the datetime column from factor to character
+bike[,"datetime"] = as.character(bike[,"datetime"])
+# then I should be able to convert the time into that format
+bike["datetime"]=as.POSIXct(bike[,"datetime"], format="%Y-%m-%d %H:%M:%OS") #the datetime column is a default date which can be called directly with this
+
+pl <- ggplot(data=bike, aes(x=datetime ,y=count))
+pl + geom_point(aes(color=factor(temp)))
+
+
 # 6.  What is the correlation between temp and count? 
+  
+  # people use this service much more when it is warm outside.
+
 # 7.  Let's explore the season data. Create a boxplot, 
 #     with the y axis indicating count and the x axis begin a box for each season.
+
+  # each boxplot corresponds to seasonal number(1:spring 2:summer 3:fall 4:winter)
+  # It does not split it up by year since that is not part of the instructions
+  # This data goes through 2 years with 4 seasons each, so it won't skew results
+pl1 <- ggplot(data=bike, aes(factor(season), count))
+pl1 + geom_boxplot()
 
 # 8.  Create an "hour" column that takes the hour from the datetime column. 
 #     You'll probably need to apply some function to the entire datetime 
@@ -45,28 +82,77 @@
 #     time.stamp <- bike$datetime[4]
 #     format(time.stamp, "%H")
 
+bike$hour <- format(bike[,"datetime"], "%H") #This appends a column named hour that is derived from datetime
+
+
 # 9.  Building the Model
 #     Use lm() to build a model that predicts count based solely on the temp 
 #     feature, name it temp.model
 
+#split test/training data
+library(caTools)
+set.seed(101)
+sample <- sample.split(bike$count, SplitRatio = 0.7)
+train = subset(bike, sample == TRUE)
+test = subset(bike, sample == FALSE)
+
+temp.model <- lm(count ~ temp, data = train)
+
 # 10. Get the summary of the temp.model
+summary(temp.model)
+
+
 # 11. How many bike rentals would we predict if the temperature was 25 
 #     degrees Celsius? Calculate this two ways:
 #     . Using the values we just got above
 #     . Using the predict() function
 
+#coefficent = 9.1320
+#intercept = 8.792
+#guess = intercept + coeff*temp
+#my result is 237.1 (rounded to the nearest whole number, count = 237)
+
 # 12. Use sapply() and as.numeric to change the hour column to a column of 
 #     numeric values.
+"hour column class before"
+class(bike[["hour"]])
+bike[["hour"]] <- sapply(bike[["hour"]], as.numeric)
+"hour column class after"
+class(bike[["hour"]])
 
 # 13. Finally build a model that attempts to predict count based off of the 
 #     following features. Figure out if theres a way to not have to 
 #     pass/write all these variables into the lm() function. 
 #     Hint: StackOverflow or Google may be quicker than the documentation.
 
+
+#season
+#holiday
+#workingday
+#weather
+#temp
+#humidity
+#windspeed
+#hour (factor)
+
+bike[["hour"]] <- factor(bike[["hour"]])
+
+#The data is already split, so I can directly call lm from here
+model <- lm(count ~ season + holiday + workingday + temp + humidity + windspeed + hour, data = train)
+
 # 14. Get the summary of the model
+summary(model)
 
 # 15. Did the model perform well on the training data? What do you think 
 #     about using a Linear Model on this data? 
+bike.predictions <- predict(model, test)
+result <- cbind(bike.predictions, test$count)
+colnames(result) <- c('predicted', 'actual')
+result <- as.data.frame(result)
+print(head(result))
+
+  # This is an awful model the predicted outcomes are not even close to the actual values
+  # a linear model doesn't reflect the correlation between some of the data so it throws the predictions off
 
 #     You should have noticed that this sort of model doesn't work well 
 #     given our seasonal and time series data. We need a model that can 
